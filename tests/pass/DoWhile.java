b192ec062038b3a31a3b1afb3b676649f673aeab
flyboyb29 <flyboyb29@gmail.com>
2016-05-30 21:18:06 -0700

    for/do loop

---
+++ tests/pass/DoWhile.java
@@ -0,0 +1,24 @@
+
+package pass;
+
+import java.lang.System;
+
+public class DoWhile {
+
+	public static void doWhile (int times) {
+		int count = 0;
+		
+		do {
+			System.out.println(count);
+			count++;
+		} while (count < times);
+	}
+	
+	public static void main(String[] args) {
+		
+		doWhile(n);
+
+	}
+	
+	static int n = 5;
+}
---
+++ tests/pass/ForLoop.java
@@ -0,0 +1,18 @@
+package pass;
+
+public class ForLoop {
+
+	public static void forLoop(int count) {
+		int i;
+		
+		for (i = 0; i <= count; i++) {
+			System.out.println("Hello World");
+		}
+	}
+	
+	public static void main(String[] args) {
+		forLoop(n);
+	}
+	
+	static int n = 5;
+}
